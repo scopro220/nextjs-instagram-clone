1d897961a355f7c951e817263beaf16a9758fcd1
scott <scott.provence@yahoo.com>
2021-12-28 11:13:02 -0500

    Stories component complete

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -10,7 +10,7 @@ export default function Stories() {
       .then(data => setSuggestions(data.results))
   }, []);
   return (
-    <div>
+    <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
       {suggestions?.map((profile) => (
         <Story
           key={profile.login.uuid}
