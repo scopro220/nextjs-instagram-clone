d8316514e22e8d58d57f913ad98c525cf068d8ad
scott <scott.provence@yahoo.com>
2021-12-28 10:44:14 -0500

    Update from faker to randomuser generator and implement Stories

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
+import Story from "./Story";
+
 export default function Stories() {
-  return <div></div>;
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    // Generate Fake Data
+    fetch('https://randomuser.me/api/?results=15')
+      .then(res => res.json())
+      .then(data => setSuggestions(data.results))
+  }, []);
+  return (
+    <div>
+      {suggestions?.map((profile) => (
+        <Story
+          key={profile.login.uuid}
+          img={profile.picture.thumbnail}
+          username={profile.login.username}
+        />
+      ))}
+    </div>
+  );
 }
