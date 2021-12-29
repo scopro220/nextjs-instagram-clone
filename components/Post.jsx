5a0f24a98fa2914e224cae9f2aeb75fcd2111f42
scott <scott.provence@yahoo.com>
2021-12-29 10:14:55 -0500

    Image and Icon section of Post

--- components/Post.jsx
+++ components/Post.jsx
@@ -10,8 +10,7 @@ import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
 export default function Post({ id, username, userImg, img, caption }) {
   return (
-    <div>
-      {/* Header */}
+    <div className="bg-white my-7 border rounded-sm">
       <div className="flex items-center p-5">
         <img
           src={userImg}
@@ -21,8 +20,18 @@ export default function Post({ id, username, userImg, img, caption }) {
         <p className="flex-1 font-bold">{username}</p>
         <DotsHorizontalIcon className="h-5" />
       </div>
-      {/* Image */}
+      <img src={img} alt={username} className="object-cover w-full" />
+
       {/* Caption */}
+      <div className="flex justify-between">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+
       {/* Comments */}
       {/* Input Box */}
     </div>
