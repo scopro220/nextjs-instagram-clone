fe4367d36a3eb9267829a7bd088428d347ca766c
scott <scott.provence@yahoo.com>
2021-12-30 17:44:42 -0500

    Customize signin page'

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,15 +1,33 @@
 import { getProviders, signIn } from "next-auth/react";
+import Header from "../../components/Header";
 
 export default function SignInPage({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img
+          src="https://links.papareact.com/ocw"
+          alt="Instagram"
+          className="w-80"
+        />
+        <p className="font-sm italic">
+          This is not a REAL app, it is a clone and built for EDUCATION PURPOSES
+          ONLY
+        </p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                className="p-3 bg-blue-500 rounded-lg text-white"
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
