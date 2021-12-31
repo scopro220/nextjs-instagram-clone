6ad6baba4e14a97c0dcaac253dad78e58e583283
scott <scott.provence@yahoo.com>
2021-12-30 20:33:47 -0500

    Build modal component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
 
 export default function Home() {
   return (
@@ -12,10 +13,9 @@ export default function Home() {
 
       <Header />
 
-      {/* Feed */}
       <Feed />
 
-      {/* Image upload Modal */}
+      <Modal />
     </div>
   );
 }
